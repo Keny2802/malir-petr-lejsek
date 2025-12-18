554b9e039d844fbedfa047d05fd8c7fd5db0ee8d
Keny2802 <oliva.vojta@gmail.com>
2025-12-18 23:29:59 +0100

    Přidání nových změn.

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -35,7 +35,7 @@ const Logo = ({ ...props }: LogoProps) => {
                     <Image
                     height={100}
                     width={100}
-                    src="/fotky/Clean Logo.png"
+                    src="/Fotky/Clean Logo.png"
                     alt="Malby Petr Lejska | Malíř Petr Lejska Logo"
                     loading="lazy"
                     decoding="async"
