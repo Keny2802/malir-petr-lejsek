66dda498b2390bdc751ab5b887dd12b636e6944f
Keny2802 <oliva.vojta@gmail.com>
2026-01-27 18:21:56 +0100

    Přidané úpravy.

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -85,13 +85,13 @@ const Hero = () => {
                         ].map((cardItem, index) => {
                                 return (
                                     <Fragment key={index}>
-                                        <Wrapper className="flex justify-center items-center gap-2 md:gap-2.5 p-1 md:p-3 lg:p-4 bg-black/50 w-full md:w-max rounded-md">
+                                        <Wrapper className="flex items-center gap-2 md:gap-2.5 p-1 md:p-3 lg:p-4 bg-black/50 w-full md:w-max rounded-md">
                                             <Wrapper>
                                                 <Icon>
                                                     <CheckCircleIcon className="text-[#a11106]" />
                                                 </Icon>
                                             </Wrapper>
-                                            <Wrapper className="flex items-start gap-2 flex-col text-start mx-auto">
+                                            <Wrapper className="flex items-start gap-2 flex-col text-start">
                                                 <h3 className="text-lg md:text-xl font-bold">
                                                     {cardItem.heading}
                                                 </h3>
